3feae8f08053be34d00ff9c6b56a7ca7e06dac37
Manfred <Man-fred@users.noreply.github.com>
2017-10-27 08:57:49 +0200

    barcode test activated

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -11,7 +11,7 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
+$$(document).on('deviceready', function () {
     console.log("Device is ready!");
 });
 
@@ -41,80 +41,59 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     myApp.alert('Here comes About page');
 });
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // `load`, `deviceready`, `offline`, and `online`.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('scan').addEventListener('click', this.scan, false);
-        document.getElementById('encode').addEventListener('click', this.encode, false);
-    },
+$$('.scan').on('click', function () {
+    console.log('scanning');
 
-    // deviceready Event Handler
-    //
-    // The scope of `this` is the event. In order to call the `receivedEvent`
-    // function, we must explicity call `app.receivedEvent(...);`
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
+    var scanner = cordova.plugins.barcodeScanner;
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    scanner.scan(function (result) {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
 
-        console.log('Received Event: ' + id);
-    },
-
-    scan: function() {
-        console.log('scanning');
-        
-        var scanner = cordova.plugins.barcodeScanner;
-
-        scanner.scan( function (result) { 
-
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
+        console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
+        document.getElementById("info").innerHTML = result.text;
+        console.log(result);
+        /*
+         if (args.format == "QR_CODE") {
+         window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+         }
+         */
 
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
-    },
+    }, function (error) {
+        console.log("Scanning failed: ", error);
+    });
+});
+$$('.encode').on('click', function () {
+    var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-    encode: function() {
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
-
+    scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function (success) {
+        alert("encode success: " + success);
+    }, function (fail) {
+        alert("encoding failed: " + fail);
     }
+    );
 
-};
+});
+
+$$('.ac-1').on('click', function () {
+    var buttons = [
+        {
+            text: 'Button1',
+            bold: true
+        },
+        {
+            text: 'Button2'
+        },
+        {
+            text: 'Cancel',
+            color: 'red'
+        },
+    ];
+    myApp.actions(buttons);
+});
