b4a417a17d51f805a554f5e355e7066535b60d46
Manfred <Man-fred@users.noreply.github.com>
2018-06-23 12:50:36 +0200

    Delete upload.php

--- www/api/upload.php
+++
@@ -1,9 +0,0 @@
-﻿
-<!-- Die Encoding-Art enctype MUSS wie dargestellt angegeben werden -->
-<form enctype="multipart/form-data" action="/api/request-upload.php" method="POST">
-    <!-- MAX_FILE_SIZE muss vor dem Dateiupload Input Feld stehen -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="100000000" />
-    <!-- Der Name des Input Felds bestimmt den Namen im $_FILES Array -->
-    Diese Datei hochladen: <input name="userfile" type="file" />
-    <input type="submit" value="Send File" />
-</form>
